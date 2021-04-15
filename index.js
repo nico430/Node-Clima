144213f460995983b4c02908e967cf3f741b5c4b
nicolas <nico.chabanpavon@gmail.com>
2021-04-15 19:53:51 -0300

    peticion get de ejemplo con axios

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer")
-
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 
 
 const main = async() => {
 
+    const busquedas = new Busquedas();
+
     let opt;
 
     do {
-
         opt = await inquirerMenu();
-        pausa()
+
+        switch (opt) {
+
+            case 1:
+                //Mostrar mensaje
+                const lugar = await leerInput('Ciudad: ');
+                busquedas.ciudad(lugar)
+                    //buscar los lugares
+
+                //seleccionar el lugar
+
+                //clima
+
+                //mostrar resultados
+
+                console.log('\n Información del lugar \n'.green);
+                console.log('Ciudad: ', );
+                console.log('Latitud: ', );
+                console.log('Longitud: ', );
+                console.log('Temperatura: ', );
+                console.log('Mínima: ', );
+                console.log('Máxima: ', );
+
+                break;
+        }
+
+        await pausa()
     } while (opt !== 0);
 
 }
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,4 +1,27 @@
+const axios = require('axios');
+
+
 class Busquedas {
+
+    Historial = ['bs', 'gr', 'g', 'p', 'ca', 'ar'];
+
+    constructor() {
+
+        //leer DB si existe
+
+    }
+
+    async ciudad(lugar = '') {
+
+        //realizar peticion http
+        //console.log(lugar);
+
+        const resp = await axios.get('https://reqres.in/api/users?page=2');
+        console.log(resp.data);
+
+        return [] // retornar lugares que coincidan
+
+    }
 
 }
 
