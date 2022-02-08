58b95f0485ed008432c91a3fb9e1ed92242c3b6f
Nicolas Chaban <nicolas@enve.io>
2022-02-08 12:20:14 -0300

    OK: mostrando resultado de petición y areglado error al cancelar

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-
-
 const { leerInput, inquirerMenu, pausa, listarLugares } = require("./helpers/inquirer");
 const Busquedas = require("./models/busquedas");
 
@@ -20,28 +18,33 @@ const main = async() => {
             case 1:
                 //Mostrar mensaje
                 const termino = await leerInput('Ciudad: ');
-
                 //Buscar ciudades
                 const lugares = await busquedas.ciudad(termino);
-
                 //seleccionar el lugar
                 const id = await listarLugares(lugares);
+
+                if(id === '0') continue;
+
                 const lugarSel = lugares.find(l => l.id === id);
-                console.log(lugarSel);
+                // guardar historial
+                busquedas.agregarHistorial(lugarSel.nombre)
 
                 //clima
-
+                const clima = await busquedas.ClimaLugar(lugarSel.latitud,lugarSel.longitud)
                 //mostrar resultados
-
+                console.clear()
                 console.log('\n Información del lugar \n'.green);
-                console.log('Ciudad: ', lugarSel.nombre);
+                console.log('Ciudad: ', lugarSel.nombre.green);
                 console.log('Latitud: ', lugarSel.latitud);
                 console.log('Longitud: ', lugarSel.longitud);
-                console.log('Temperatura: ', );
-                console.log('Mínima: ', );
-                console.log('Máxima: ', );
+                console.log('Temperatura: ', clima.temp );
+                console.log('Mínima: ', clima.min);
+                console.log('Máxima: ', clima.max);
+                console.log('Como está el clima: ', clima.desc);
 
                 break;
+            
+            
         }
 
         await pausa()
